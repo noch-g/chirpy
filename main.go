b11b00466be60363a94346bbb1f1eeeb93a94aa4
noch-g <ladle.doings_0g@icloud.com>
2025-02-16 08:37:47 +0100

    feat: add healthz endpoint

--- main.go
+++ main.go
@@ -1,14 +1,28 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	serveMux := http.NewServeMux()
-	serveMux.Handle("/", http.FileServer(http.Dir("./")))
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: serveMux,
+	const filepathRoot = "."
+	const port = "8080"
+
+	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir(filepathRoot))))
+
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(srv.ListenAndServe())
 }
